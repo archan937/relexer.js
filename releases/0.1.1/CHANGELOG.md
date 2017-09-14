974bf36c2759891a4ee6a0908b5d738e47059713
Paul Engel <paulengel@bettyblocks.com>
2017-09-14 22:44:02 +0200

    Released reLexer.js v0.1.2

--- releases/0.1.1/CHANGELOG.md
+++ releases/0.1.1/CHANGELOG.md
@@ -1,5 +1,9 @@
 ## reLexer.js CHANGELOG
 
+### Version 0.1.1 (September 9, 2017)
+
+* Provide rule precedence (which fixes failing ternary expression ^^)
+
 ### Version 0.1.0 (August 1, 2017)
 
 * Initial release
---
+++ releases/0.1.2/demo/lexer.js
@@ -0,0 +1,140 @@
+var
+  rules = {
+    space: /\s+/,
+    path: /[a-zA-Z](\w*)\.?([\w+\.]+)?/,
+    string: /(["'])(?:(?=(\\?))\2.)*?\1/,
+    number: /-?\d+(\.\d+)?/,
+    boolean: /(true|false)/,
+    primitive: ':boolean|:number|:string|:path',
+    multiplyDivideExpression: [
+      ':expression>left',
+      ':space?',
+      '*|/>operator',
+      ':space?',
+      ':expression>right'
+    ],
+    addSubtractExpression: [
+      ':expression>left',
+      ':space?',
+      '+|->operator',
+      ':space?',
+      ':expression>right'
+    ],
+    comparisonExpression: [
+      ':expression>left',
+      ':space?',
+      '<|<=|==|!=|>=|>>operator',
+      ':space?',
+      ':expression>right'
+    ],
+    logicalOperator: or(
+      '&&',
+      '||'
+    ),
+    logicalExpression: [
+      ':expression>left',
+      ':space?',
+      ':logicalOperator>operator',
+      ':space?',
+      ':expression>right'
+    ],
+    ternaryExpression: [
+      ':expression>statement',
+      ':space?', '?', ':space?',
+      ':expression>true&',
+      ':space?', ':', ':space?',
+      ':expression>false&'
+    ],
+    encapsulation: [
+      '(', ':space?', ':expression>expression&', ':space?', ')'
+    ],
+    expression: or(
+      ':encapsulation',
+      ':ternaryExpression/1',
+      ':logicalExpression/2',
+      ':comparisonExpression/3',
+      ':addSubtractExpression/4',
+      ':multiplyDivideExpression/5',
+      ':primitive'
+    )
+  },
+  binaryExpression = function(env, captures) {
+    var
+      left = captures.left,
+      operator = captures.operator,
+      right = captures.right;
+
+    switch(operator) {
+    case '&&':
+      return left && right;
+    case '||':
+      return left || right;
+    case '+':
+      return left + right;
+    case '-':
+      return left - right;
+    case '*':
+      return left * right;
+    case '/':
+      return left / right;
+    case '<':
+      return left < right;
+    case '<=':
+      return left <= right;
+    case '==':
+      return left == right;
+    case '!=':
+      return left != right;
+    case '>':
+      return left > right;
+    case '>=':
+      return left >= right;
+    }
+  },
+  actions = {
+    path: function(env, path) {
+      var
+        properties = path.split('.'),
+        value = env,
+        i, p;
+
+      for (i = 0; i < properties.length; i++) {
+        p = properties[i];
+        if (value && value.hasOwnProperty(p))
+          value = value[p];
+        else
+          return;
+      }
+
+      return value;
+    },
+    string: function(env, string) {
+      return JSON.parse(string);
+    },
+    number: function(env, number) {
+      var type = number.match(/\./) ? 'Float' : 'Int';
+      return Number['parse' + type](number);
+    },
+    boolean: function(env, bool) {
+      return bool == 'true';
+    },
+    ternaryExpression: function(env, captures) {
+      return captures.statement ? captures.true : captures.false;
+    },
+    logicalExpression: function(env, captures) {
+      return binaryExpression(env, captures);
+    },
+    comparisonExpression: function(env, captures) {
+      return binaryExpression(env, captures);
+    },
+    multiplyDivideExpression: function(env, captures) {
+      return binaryExpression(env, captures);
+    },
+    addSubtractExpression: function(env, captures) {
+      return binaryExpression(env, captures);
+    },
+    encapsulation: function(env, captures) {
+      return captures.expression;
+    }
+  },
+  lexer = new reLexer(rules, 'expression', actions);
